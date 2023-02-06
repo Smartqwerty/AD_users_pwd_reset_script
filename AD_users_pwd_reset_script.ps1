193d68d8a3143235686ada434b50b1378f7ee1d6
Smartqwertyq <oleinik-dv@ya.ru>
2023-02-06 14:59:55 +0300

    Update AD_users_pwd_reset_script.ps1

--- AD_users_pwd_reset_script.ps1
+++ AD_users_pwd_reset_script.ps1
@@ -3,9 +3,14 @@ $TEST_SCRIPT = $True
 #users in $excluded_users - will be excluded from pwd reset
 $group_to_reset_name = 'password_hard'
 $group_to_exclude_name = 'password_excluded_users'
+#old pwd days count
+$days = 20
+$days = [DateTime]::Today.AddDays(-$days)
+"{0} {1}" -f 'Change all passwords older:', $days
 #check admin priveleges
 $currPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
 $Admin = $currPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
 
 if ($Admin)
 {
@@ -21,19 +26,30 @@ if ($Admin)
         {
          if ($user.SamAccountName -eq $ex_user.SamAccountName)
          {
-          $ex = $True
-          break
+             $ex = $True
+             break
          }     
         }
     if ($ex -ne $True)
         {
-        if ($TEST_SCRIPT -eq $True)
-            {
-                $user.SamAccountName
+        if ($TEST_SCRIPT -eq $True) #print user & PasswordLastSet date
+            {               
+                $user = $user | Get-ADUser -Properties * # PasswordLastSet, PasswordExpired
+                $uDate = $user.PasswordLastSet 
+                if (($uDate -lt $days) -and ($user.Enabled -eq $true))
+                {
+                    "{0} | {1}" -f $user.SamAccountName, $uDate
+                }
             }
-        else 
+        else #change password
             {
-                $user | Set-ADuser -ChangePasswordAtLogon $True
+                $user = $user | Get-ADUser -Properties PasswordLastSet
+                $uDate = $user.PasswordLastSet
+                #Force to change
+                if ($uDate -lt $days)
+                {
+                    $user | Set-ADuser -ChangePasswordAtLogon $True   
+                }
             }
         }
     }
